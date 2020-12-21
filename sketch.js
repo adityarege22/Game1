ab0b5a727bfcdde40196adc5dff7964ec171ea5b
adityarege22 <70090303+adityarege22@users.noreply.github.com>
2020-12-21 15:01:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var maze1, maze2, maze3, maze4, maze5;
+var maze6, maze7, maze8, maze9, maze10;
+var maze11, maze12, maze13, maze14, maze15;
+var maze16, maze17, maze18, maze19, maze10;
+var maze21, maze22, maze23, maze24, maze25;
+
+
+function preload(){
+
+
+
+}
+
+
+function setup(){
+createCanvas(800,700);
+
+maze1 = createSprite(70,50,20,100)
+maze1.shapeColor="brown";
+
+maze2 = createSprite(410,15,700,20);
+maze2.shapeColor="brown";
+
+maze3 = createSprite(750,270,20,500)
+maze3.shapeColor="brown";
+
+maze4 = createSprite(410,560,700,20)
+maze4.shapeColor="brown";
+
+maze5 = createSprite(70,400,20,300)
+maze5.shapeColor="brown";
+
+maze6 = createSprite(180,260,200,20)
+maze6.shapeColor="brown";
+
+maze7 = createSprite(70,50,20,100)
+maze7.shapeColor="brown";
+
+maze8 = createSprite(150,400,20,150)
+maze8.shapeColor="brown";
+
+maze9 = createSprite(200,465,100,20)
+maze9.shapeColor="brown";
+
+maze10 = createSprite(240,510,20,100)
+maze10.shapeColor="brown";
+
+maze11 = createSprite(240,370,20,80)
+maze11.shapeColor="brown";
+
+maze12 = createSprite(300,400,100,20)
+maze12.shapeColor="brown";
+
+maze13 = createSprite(340,480,20,150)
+maze13.shapeColor="brown";
+
+maze14 = createSprite(370,340,150,20)
+maze14.shapeColor="brown";
+
+maze15 = createSprite(450,430,20,200)
+maze15.shapeColor="brown";
+
+maze16 = createSprite(550,300,20,350)
+maze16.shapeColor="brown";
+
+maze17 = createSprite(580,200,80,20)
+maze17.shapeColor="brown";
+
+
+maze18 = createSprite(580,350,80,20)
+maze18.shapeColor="brown";
+
+maze19 = createSprite(610,460,20,200)
+maze19.shapeColor="brown";
+
+maze20 = createSprite(700,270,100,20)
+maze20.shapeColor="brown";
+
+maze21 = createSprite(700,125,100,20)
+maze21.shapeColor="brown";
+
+
+
+}
+
+
+function draw(){
+background("white");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  drawSprites();
+}
